74c22bc755475b09beb0dc57e56c510fc9e40939
Martin Paakspuu <martin.paakspuu@itcollege.ee>
2019-02-23 21:07:56 +0200

    renamed main class

--- src/AnagrammFinder.java
+++ src/AnagrammFinder.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Main {
+public class AnagrammFinder {
 
     public static void main(String[] args) throws IOException {
         long startTime = System.nanoTime();
