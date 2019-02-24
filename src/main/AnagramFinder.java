ce4dfba609d57af9f9bb49c1a1207d08b9360db4
Martin Paakspuu <martin.paakspuu@itcollege.ee>
2019-02-24 14:27:15 +0200

    faster solution

--- src/main/AnagramFinder.java
+++ src/main/AnagramFinder.java
@@ -1,3 +1,5 @@
+package main;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -6,16 +8,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-public class AnagrammFinder {
+public class AnagramFinder {
 
     public static void main(String[] args) throws IOException {
         long startTime = System.nanoTime();
 
         String path = args[0];
         String word = args[1];
-        List<String> anagrams = getAnagramsFromTextFile(word, path);
+        List<String> anagrams = findAnagramsFromTextFile(word, path);
 
         printResult(startTime, anagrams);
     }
@@ -24,15 +25,6 @@ public class AnagrammFinder {
         double stop = (System.nanoTime() - startTime) / 1000d;
         result.add(0, Double.toString(stop));
         System.out.println(String.join(",", result));
-    }
-
-    private static List<String> getAnagramsFromTextFile(String target, String path) throws IOException {
-        Map<Character, Integer> characterMap = createCharacterMap(target);
-        List<String> words = getWordsWithLengthFromFile(target.length(), path);
-        return words.stream()
-                .filter(element -> containsSameCharacters(characterMap, element))
-                .filter(element -> isAnagram(target, element))
-                .collect(Collectors.toList());
     }
 
     private static boolean isAnagram(String target, String element) {
@@ -49,15 +41,6 @@ public class AnagrammFinder {
         return true;
     }
 
-    private static boolean containsSameCharacters(Map<Character, Integer> characterMap, String element) {
-        for (int i = 0; i < element.length(); i++) {
-            if (characterMap.get(element.charAt(i)) == null) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     private static Map<Character, Integer> createCharacterMap(String target) {
         Map<Character, Integer> characterMap = new HashMap<>();
         for (int i = 0; i < target.length(); i++) {
@@ -68,16 +51,17 @@ public class AnagrammFinder {
         return characterMap;
     }
 
-    private static List<String> getWordsWithLengthFromFile(int length, String path) throws IOException {
+    private static List<String> findAnagramsFromTextFile(String target, String path) throws IOException {
         List<String> words = new ArrayList<>();
         File file = new File(path);
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String line;
         while ((line = reader.readLine()) != null) {
-            if (line.length() == length)
+            if (line.length() == target.length() && isAnagram(target, line))
                 words.add(line);
         }
         return words;
     }
+
 
 }
